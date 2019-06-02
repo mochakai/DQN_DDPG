b9749ee7f993e3de788c51ced884b764b263a448
KAI_desktop <ms0346648@gmail.com>
2019-06-02 17:34:18 +0800

    - add some annotate

--- src/show_result.py
+++ src/show_result.py
@@ -14,6 +14,14 @@ def show_result(res_dict):
     plt.plot(mean)
     plt.xlabel('Episode')
     plt.ylabel('total_reward')
+    
+    ymax = max(mean)
+    xpos = mean.index(ymax)
+    arrowprops=dict(arrowstyle="->",connectionstyle="angle,angleA=0,angleB=60")
+    bbox_props = dict(boxstyle="square", pad=0.3, fc="w", ec="k", lw=0.72)
+    plt.annotate('Average max: {}'.format(ymax), 
+                  xy=(xpos, ymax), xytext=(xpos, ymax+5000),
+                  arrowprops=arrowprops, bbox=bbox_props)
     plt.text(len(mean)-1, mean[-1], str(mean[-1]))
     plt.show()
 
